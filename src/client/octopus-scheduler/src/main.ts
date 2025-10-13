407e237fc87d766d68989f952b544eb70d3d653a
dev-user <your.email@example.com>
2025-10-13 15:44:39 +0000

    不要な処理を削除

--- src/client/octopus-scheduler/src/main.ts
+++ src/client/octopus-scheduler/src/main.ts
@@ -4,21 +4,11 @@ import "./style.css";
 import App from "./App.vue";
 import { Container } from "./core/container/index";
 import router from "./core/router";
-import { EventPollingService } from "./model/applications/event-polling-service";
-import { AssetService } from "./model/applications/assets/asset-service";
-import { container } from "tsyringe";
-import { useAudio } from "../../packages/shared-composables/src/use-audio";
 import { registerEventHandlers } from "./ui/components/schedule-event-handler/register-event-handlers";
 
 Container.Register();
 
 const app = createApp(App);
-const eventPollingService = new EventPollingService();
-const assetService = container.resolve<AssetService>("AssetService");
-const audio = useAudio();
-app.provide("eventPollingService", eventPollingService);
-app.provide("assetService", assetService);
-app.provide("audio", audio);
 app.use(router).mount("#app");
 
 registerEventHandlers(router);
