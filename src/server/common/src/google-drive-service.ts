633f5564296e18c246a5a44be6d721db299163bb
dev-user <your.email@example.com>
2025-10-17 15:17:24 +0000

    大幅改変中

--- src/server/common/src/google-drive-service.ts
+++ src/server/common/src/google-drive-service.ts
@@ -1,38 +1,59 @@
 interface DriveData {
-  fileId: string;
+  metadata: DriveMetadata;
   fileName: string;
   fileKind: string; // MimeType
-  fileData: string; // dataUrl
+  fileDataUrl: string; // dataUrl
   uploadDate: Date;
-  lastUpdate: Date;
+  parentFolderId: string;
 }
 
 interface DriveMetadata {
+  driveDataId: string;
   fileId: string;
+  parentFolderId: string;
   lastUpdate: Date;
 }
 
 export class GoogleDriveService {
-  addDriveData(driveData: DriveData): void {
+  addDriveData(driveData: DriveData): DriveMetadata {
     const blob = Utilities.newBlob(
-      Utilities.base64Decode(driveData.fileData),
+      Utilities.base64Decode(driveData.fileDataUrl),
       driveData.fileKind,
       driveData.fileName
     );
-    const file = DriveApp.createFile(blob);
-    file.setName(driveData.fileName);
-    // Note: Need to set parent folder if needed
+    const folder = DriveApp.getFolderById(driveData.parentFolderId);
+    const file = folder.createFile(blob);
+    file.setName(`${driveData.metadata.driveDataId}_${driveData.fileName}`);
+    return {
+      driveDataId: driveData.metadata.driveDataId,
+      fileId: file.getId(),
+      parentFolderId: file.getParents().next().getId(),
+      lastUpdate: new Date(file.getLastUpdated().getTime()),
+    };
   }
 
-  getDriveMetaData(): DriveMetadata[] {
-    const files = DriveApp.getFiles();
+  getDriveMetaData(folderId: string): DriveMetadata[] {
+    const folder = DriveApp.getFolderById(folderId);
     const metadata: DriveMetadata[] = [];
+    // Add files in current folder
+    const files = folder.getFiles();
     while (files.hasNext()) {
       const file = files.next();
+      const fileName = file.getName();
+      const driveDataId = fileName.split("_")[0];
       metadata.push({
+        driveDataId,
         fileId: file.getId(),
+        parentFolderId: folderId,
         lastUpdate: new Date(file.getLastUpdated().getTime()),
       });
+    }
+    // Recursively add files in subfolders
+    const subFolders = folder.getFolders();
+    while (subFolders.hasNext()) {
+      const subFolder = subFolders.next();
+      const subMetadata = this.getDriveMetaData(subFolder.getId());
+      metadata.push(...subMetadata);
     }
     return metadata;
   }
@@ -42,13 +63,23 @@ export class GoogleDriveService {
       const file = DriveApp.getFileById(dataId);
       const blob = file.getBlob();
       const dataUrl = Utilities.base64Encode(blob.getBytes());
-      return {
+      const fullFileName = file.getName();
+      const driveDataId = fullFileName.split("_")[0];
+      const fileName = fullFileName.split("_").slice(1).join("_");
+      const parentFolderId = file.getParents().next().getId();
+      const metadata: DriveMetadata = {
+        driveDataId,
         fileId: dataId,
-        fileName: file.getName(),
-        fileKind: file.getMimeType(),
-        fileData: dataUrl,
-        uploadDate: new Date(file.getDateCreated().getTime()),
+        parentFolderId,
         lastUpdate: new Date(file.getLastUpdated().getTime()),
+      };
+      return {
+        metadata,
+        fileName,
+        fileKind: file.getMimeType(),
+        fileDataUrl: dataUrl,
+        uploadDate: new Date(file.getDateCreated().getTime()),
+        parentFolderId,
       };
     } catch {
       return null;
@@ -66,14 +97,14 @@ export class GoogleDriveService {
 
   updateDriveData(driveData: DriveData): void {
     try {
-      const file = DriveApp.getFileById(driveData.fileId);
+      const file = DriveApp.getFileById(driveData.metadata.fileId);
       const blob = Utilities.newBlob(
-        Utilities.base64Decode(driveData.fileData),
+        Utilities.base64Decode(driveData.fileDataUrl),
         driveData.fileKind,
         driveData.fileName
       );
       file.setContent(blob.getDataAsString());
-      file.setName(driveData.fileName);
+      file.setName(`${driveData.metadata.driveDataId}_${driveData.fileName}`);
     } catch {
       // Ignore if file not found
     }
