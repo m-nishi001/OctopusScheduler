ec79a4eadacd1b0697ce963c9a34b0860aa625c6
dev-user <your.email@example.com>
2025-10-18 15:02:31 +0000

    chore: remove unused src/client/games/card-game/src/types/gas-bridge.d.ts

--- src/client/games/card-game/src/types/gas-bridge.d.ts
+++
@@ -1,8 +0,0 @@
-declare module "@common/gas" {
-  export type FinalGasCallResult<T> = any;
-  export class GasFunctionService {
-    constructor(functionName?: string, options?: any);
-    static create(apiFunctionName: string): GasFunctionService | null;
-    call<T = any>(args?: any): Promise<FinalGasCallResult<T>>;
-  }
-}
