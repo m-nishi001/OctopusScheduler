f84733ba6ae30914cd42d3a68ae579b1a2b6b5ac
dev-user <your.email@example.com>
2025-10-22 14:31:17 +0000

    Refactor router-view transition handling for improved component rendering

--- src/client/games/jackpot-game/src/App.vue
+++ src/client/games/jackpot-game/src/App.vue
@@ -1,7 +1,9 @@
 <template>
-  <transition name="fade" mode="out-in">
-    <router-view />
-  </transition>
+  <router-view v-slot="{ Component }">
+    <transition name="fade" mode="out-in">
+      <component :is="Component" />
+    </transition>
+  </router-view>
 </template>
 
 <style scoped>
