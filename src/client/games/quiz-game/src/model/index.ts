b55dc7c4844e0446de06cc4d9bbf2b0f409817a8
dev-user <your.email@example.com>
2025-11-03 06:41:01 +0000

    refactor: remove unused exports from model index files

--- src/client/games/quiz-game/src/model/index.ts
+++
@@ -1,3 +0,0 @@
-export * from "./domains";
-export * from "./applications";
-export * from "./infrastructures";
--- src/client/games/quiz-game/src/model/infrastructures/index.ts
+++
@@ -1,3 +0,0 @@
-export * from "./repositories/quiz-repository";
-export * from "./repositories/result-repository";
-export * from "./services/api-service";
