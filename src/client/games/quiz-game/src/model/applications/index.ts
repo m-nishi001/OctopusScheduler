784a1c391e342b632150ffe6a47c035b0ae4b9ff
dev-user <your.email@example.com>
2025-11-03 06:57:36 +0000

    refactor: remove unused exports from applications index file

--- src/client/games/quiz-game/src/model/applications/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./use-cases/start-quiz-use-case";
-export * from "./use-cases/stop-quiz-use-case";
-export * from "./use-cases/get-results-use-case";
-export * from "./dtos/quiz-dto";
-export * from "./dtos/result-dto";
