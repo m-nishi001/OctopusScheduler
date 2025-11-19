4f755df575529f3cd0902795a2fc1ae9cb42901f
dev-user <your.email@example.com>
2025-11-19 10:47:41 +0000

    chore(jackpot-game): update prizes barrel exports after removing deprecated prizes-upload

--- src/client/games/jackpot-game/src/ui/pages/admin/prizes/index.ts
+++ src/client/games/jackpot-game/src/ui/pages/admin/prizes/index.ts
@@ -1,1 +1,6 @@
-export * from "./prizes-upload";
+export * from "./use-prizes";
+export * from "./use-assets";
+export * from "./use-prize-sync";
+export { default as AdminPrizes } from "./admin-prizes.vue";
+export { default as PrizeAddDialog } from "./prize-add-dialog.vue";
+export { default as PrizeEditDialog } from "./prize-edit-dialog.vue";
